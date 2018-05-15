feffb22b6456940ea986e5d72d12b79811830462
sams3p10l <mix996@gmail.com>
2018-05-15 16:03:01 +0200

    now returns one output file from all images in folder

--- imgProcesser.py
+++ imgProcesser.py
@@ -1,46 +1,52 @@
 from PIL import Image
+import os
 
-inputImg = input("Image for processing: (or path to it)\n")
+iterator = 0
 
-image = Image.open(inputImg)
-image = image.convert('RGB')
-pixel = image.load()
+fileList = []
+fileList = os.listdir()
 
-pixelTuples = []
-pixelList = []
-output = ""
+for i in range(len(fileList)):
+    ext = fileList[i][-4:]
+    if  ext == '.bmp' or ext == '.jpg' or ext == '.png':
+        image = Image.open(fileList[i])
+        image = image.convert('RGB')
+        pixel = image.load()
 
-outputFilename = inputImg[:-4] + '_out.txt'
+        pixelTuples = []
+        pixelList = []
+        output = ""
 
-f = open(outputFilename, 'w')
+        f = open('output.txt', 'a+')
 
-for x_pos in range(image.size[0]):
-    for y_pos in range(image.size[1]):
-        pixelTuples.append(pixel[x_pos, y_pos])
+        for x_pos in range(image.size[0]):
+            for y_pos in range(image.size[1]):
+                pixelTuples.append(pixel[x_pos, y_pos])
 
-for i in range(len(pixelTuples)):
-    pixelList.append(list(pixelTuples[i]))      #RGBA
+        for i in range(len(pixelTuples)):
+            pixelList.append(list(pixelTuples[i]))      #RGBA
 
-cnt = 0
+        cnt = 0
 
-for i in range(len(pixelList)):
-    pixelList[i].reverse()                      #ABGR
-    for j in range(len(pixelList[i])):
-        if(cnt == 0):
-            output += str(i) + " => " + 'x"'
+        for i in range(len(pixelList)):
+            pixelList[i].reverse()                      #ABGR
+            for j in range(len(pixelList[i])):
+                if(cnt == 0):
+                    output += str(iterator) + " => " + 'x"'
+                    iterator += 1
 
-        pixelList[i][j] = format(pixelList[i][j], '02x')
-        output += str(pixelList[i][j]).upper()
+                pixelList[i][j] = format(pixelList[i][j], '02x')
+                output += str(pixelList[i][j]).upper()
+                
+                cnt += 1
+
+                if cnt == 4 and i != len(pixelList) - 1:
+                    output += '",\n'
+                    cnt = 0
+
+        output += '"\n'
+        f.write(output)
         
-        cnt += 1
-
-        if cnt == 4 and i != len(pixelList) - 1:
-            output += '",\n'
-            cnt = 0
-
-output += '"'
-f.write(output)
 f.close()
-
-print("Generated output " + outputFilename + " successfully")
+print("Output generated successfully")
    
